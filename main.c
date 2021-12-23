631049df81d79cdfecd64ab0f1bb14496721df1c
Khaled Ali <khaled21.ali33@gmail.com>
2021-12-24 00:36:45 +0200

    Fisrt version of the game is done

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
-#include <stdint.h>
 #include "main.h"
 uint8_t board[9];
 uint8_t turn;
 uint8_t gameStatus;
 void main(void){
+    srand(time(NULL)); //to generate different sequences
     initBoard();
     printBoard();
+    while(gameStatus){
+        gameMove();
+    }
+}
+
+void initBoard(void){
+    //Clear Board and Ask for whom to start
+    uint8_t index = 0;
+    for(index = 0; index < 9; ++index)
+    {
+        board[index] = '-';
+    }
     uint8_t ans;
+    gameStatus = GAMEON;
     printf("Do you want to play first? [Y/N]");
-    do
+    while (1)
     {
         scanf("%c", &ans);
         if(ans == 'Y' || ans == 'y'){
@@ -23,25 +35,114 @@ void main(void){
         else{
             printf("Invalid answer, please answer with Y or N.");
         }
-    } while (1);
-
-    while(gameStatus){
-        gameMove(turn);
-    }
-}
-
-void initBoard(void){
-    uint8_t index = 0;
-    for(index = 0; index < 9; ++index)
-    {
-        board[index] = '-';
     }
 
 }
 void printBoard(void){
     int index = 0;
+    printf("Board: \n");
     for(index = 0; index < 9; index+=3)
     {
         printf("\n%c | %c | %c\n", board[index], board[index+1], board[index+2]);
     }
+}
+void gameMove(void){
+    int8_t move = -1;
+    if(turn == PLAYER){
+    while (move < 1 || move > 9)
+        {
+           getPlayerMove(&move);
+        }
+        board[move-1] = 'O';
+    }
+    else if(turn == COMPUTER){
+        while (move < 1 || move > 9)
+        {
+            move = getComputerMove();
+        }
+        board[move] = 'X';
+    }
+    printBoard();
+    int8_t status = checkForWin();
+    switch (status)
+    {
+    case PLAYER:
+        printf("You Win.");
+        gameStatus = GAMEOVER;
+        break;
+    case COMPUTER:
+        printf("You Lose.");
+        gameStatus = GAMEOVER;
+        break;
+    case DRAW:
+        printf("The game is a Draw.");
+        gameStatus = GAMEOVER;
+        break;
+    default:
+        turn = !turn;
+        break;
+    }
+}
+void getPlayerMove(int8_t* playerMove){
+        printf("Please Enter a move from 1 to 9: ");
+        while (1)
+        {
+            scanf("%d", playerMove);
+            fflush(stdin);
+            if(*playerMove < 1 || *playerMove > 9){
+                printf("Move out of range, Please Enter a move from 1 to 9: ");
+            }
+            else if (board[*playerMove-1] != '-')
+            {
+                printf("Square is not empty, Please Enter a different move from 1 to 9: ");
+            }
+            else{
+                break;
+            }         
+        }
+}
+int8_t checkForWin(void){
+    // int8_t winner = (turn == PLAYER? 'O' : 'X');
+    //check for 3 in a row
+    int8_t index = 0;
+    for(index = 0; index < 9; index += 3){
+        if(board[index] != '-' && board[index] == board[index + 1] && board[index] == board[index + 2])
+        {
+            return board[index] == 'O'? PLAYER : COMPUTER;
+        }
+    }
+    //Check for 3 in a column
+    for(index = 0; index < 3; index++){
+        if(board[index] != '-' && board[index] == board[index + 3] && board[index] == board[index + 6])
+        {
+            return board[index] == 'O'? PLAYER : COMPUTER;
+        }
+    }
+    //check for 3 in a diagonal
+    if(board[4] != '-' &&
+     ((board[4] == board[0] && board[4] == board[8])|| (board[4] == board[2] && board[4] == board[6]))){
+        return board[4] == 'O'? PLAYER : COMPUTER;
+     }
+    //check for draw
+    for(index = 0; index < 9; index++){
+        if(board[index] == '-'){
+            return 2;
+        }
+    }
+    return DRAW;
+}
+int8_t getComputerMove(void){
+    uint8_t availableMoves[9];
+    uint8_t index = 0;
+    int8_t computerMove = -1;
+    uint8_t lastEmpty = 0;
+    for(index = 0; index < 9; index++){
+        if(board[index] == '-'){
+            availableMoves[lastEmpty++] = index;
+        }
+    }
+    //Dummy Computer
+    computerMove = rand() % lastEmpty;
+    computerMove = availableMoves[computerMove];
+    return computerMove;
 }
--- main.h
+++ main.h
@@ -1,7 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
 #define PLAYER 0
 #define COMPUTER 1
 #define GAMEON 1
 #define GAMEOVER 0
+#define DRAW -1
 void initBoard(void);
 void printBoard(void);
-void gameMove(uint8_t);
+void gameMove(void);
+void getPlayerMove(int8_t*);
+int8_t checkForWin(void);
+int8_t getComputerMove(void);
